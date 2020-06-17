8380ec9f6b16fd90731ff55cc8aed122d3ce3007
afope <afopefoluwa@unchain.io>
2020-06-17 21:35:43 +0200

    format code

--- project-6/contracts/coffeeaccesscontrol/CreatorRole.sol
+++ project-6/contracts/coffeeaccesscontrol/CreatorRole.sol
@@ -5,50 +5,50 @@ import "./Roles.sol";
 
 // Define a contract 'CreatorRole' to manage this role - add, remove, check
 contract CreatorRole {
-  using Roles for Roles.Role;
+    using Roles for Roles.Role;
 
-  // Define 2 events, one for Adding, and other for Removing
-  event CreatorAdded(address indexed account);
-  event CreatorRemoved(address indexed account);
+    // Define 2 events, one for Adding, and other for Removing
+    event CreatorAdded(address indexed account);
+    event CreatorRemoved(address indexed account);
 
-  // Define a struct 'creators' by inheriting from 'Roles' library, struct Role
-  Roles.Role private creators;
+    // Define a struct 'creators' by inheriting from 'Roles' library, struct Role
+    Roles.Role private creators;
 
-  // In the constructor make the address that deploys this contract the 1st creator
-  constructor() public {
-    _addCreator(msg.sender);
-  }
+    // In the constructor make the address that deploys this contract the 1st creator
+    constructor() public {
+        _addCreator(msg.sender);
+    }
 
-  // Define a modifier that checks to see if msg.sender has the appropriate role
-  modifier onlyCreator() {
-    require(isCreator(msg.sender));
-    _;
-  }
+    // Define a modifier that checks to see if msg.sender has the appropriate role
+    modifier onlyCreator() {
+        require(isCreator(msg.sender));
+        _;
+    }
 
-  // Define a function 'isCreator' to check this role
-  function isCreator(address account) public view returns (bool) {
-    return creators.has(account);
-  }
+    // Define a function 'isCreator' to check this role
+    function isCreator(address account) public view returns (bool) {
+        return creators.has(account);
+    }
 
-  // Define a function 'addCreator' that adds this role
-  function addCreator(address account) public onlyCreator {
-    _addCreator(account);
-  }
+    // Define a function 'addCreator' that adds this role
+    function addCreator(address account) public onlyCreator {
+        _addCreator(account);
+    }
 
-  // Define a function 'renounceCreator' to renounce this role
-  function renounceCreator() public {
-    _removeCreator(msg.sender);
-  }
+    // Define a function 'renounceCreator' to renounce this role
+    function renounceCreator() public {
+        _removeCreator(msg.sender);
+    }
 
-  // Define an internal function '_addCreator' to add this role, called by 'addCreator'
-  function _addCreator(address account) internal {
-    creators.add(account);
-    emit CreatorAdded(account);
-  }
+    // Define an internal function '_addCreator' to add this role, called by 'addCreator'
+    function _addCreator(address account) internal {
+        creators.add(account);
+        emit CreatorAdded(account);
+    }
 
-  // Define an internal function '_removeCreator' to remove this role, called by 'removeCreator'
-  function _removeCreator(address account) internal {
-    creators.remove(account);
-    emit CreatorRemoved(account);
-  }
+    // Define an internal function '_removeCreator' to remove this role, called by 'removeCreator'
+    function _removeCreator(address account) internal {
+        creators.remove(account);
+        emit CreatorRemoved(account);
+    }
 }
